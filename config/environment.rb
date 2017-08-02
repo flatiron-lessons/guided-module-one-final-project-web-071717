50dbc4c70824e8747b58dd3d0f265085ba15f770
github username <github email address>
2017-08-02 11:12:02 -0400

    Turned off SQL Logger

--- config/environment.rb
+++ config/environment.rb
@@ -8,4 +8,5 @@ Bundler.require
 require_all "app"
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger.level = 1
 require_all 'lib'
